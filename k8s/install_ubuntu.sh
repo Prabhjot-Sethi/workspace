0bbf2670336c6d662d5b598af811765a8381a6c5
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2021-02-01 11:26:02 +0000

    Add script for kubeadm install

---
+++ k8s/install_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y linux-headers-$(uname -r) docker.io
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00
+
+# setup nodes based on thier roles
+#sudo kubeadm init
