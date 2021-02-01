8d4d1a822979a0084a87ffe21d32b23c60083d21
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2021-02-01 18:51:00 +0530

    Update install-tf.sh

--- tungsten/install-tf.sh
+++ tungsten/install-tf.sh
@@ -7,6 +7,8 @@ sed -i -e "s/__NODE_IP__/$1/g" $SCRIPT_DIR/tf-manifest.yaml
 kubectl apply -f $SCRIPT_DIR/tf-manifest.yaml
 
 MASTER=$(kubectl get nodes | grep master | awk '{print $1}')
+
+# add labels for master node
 kubectl label node $MASTER node-role.opencontrail.org/config=
 kubectl label node $MASTER node-role.opencontrail.org/configdb=
 kubectl label node $MASTER node-role.opencontrail.org/control=
@@ -16,4 +18,7 @@ kubectl label node $MASTER node-role.opencontrail.org/analytics_alarm=
 kubectl label node $MASTER node-role.opencontrail.org/analytics_snmp=
 kubectl label node $MASTER node-role.opencontrail.org/analyticsdb=
 kubectl label node $MASTER node-role.opencontrail.org/webui=
+
+# taint master node
+kubectl taint nodes $MASTER node-role.kubernetes.io/master-
 
