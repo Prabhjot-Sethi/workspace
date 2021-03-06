7be4f0093d20344d67ac7f41cc888910f956fa86
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2021-03-06 15:32:28 +0530

    Update k8s-init.sh

--- k8s/k8s-init.sh
+++ k8s/k8s-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo kubeadm init --token-ttl 0 --kubernetes-version v1.20.0 --pod-network-cidr 10.32.0.0/12 --service-cidr 10.96.0.0/12
+KUBE_VERSION=${KUBE_VERSION:-1.18.3}
+
+sudo kubeadm init --token-ttl 0 --kubernetes-version v${KUBE_VERSION} --pod-network-cidr 10.32.0.0/12 --service-cidr 10.96.0.0/12
 
 mkdir -p $HOME/.kube
 sudo cp -u /etc/kubernetes/admin.conf $HOME/.kube/config
