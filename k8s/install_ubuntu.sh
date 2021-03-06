ab941a4c7b1baa35c141bd6d89c3182aaae20a8c
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2021-03-06 15:31:45 +0530

    Update install_ubuntu.sh

--- k8s/install_ubuntu.sh
+++ k8s/install_ubuntu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+KUBE_VERSION=${KUBE_VERSION:-1.18.3}
+
 sudo apt-get update -y
 sudo apt-get install -y linux-headers-$(uname -r) docker.io
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
-sudo apt-get install -y kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00
+sudo apt-get install -y kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00
 
 # setup nodes based on thier roles
 #sudo kubeadm init
